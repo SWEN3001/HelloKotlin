3e5cb00478551b75e1a73d9561896ae668040de1
Antonio Fearon <antonio.l.fearon@gmail.com>
2022-10-26 15:00:51 -0500

    Update MainActivity.kt
    Refactored to use view binding

--- app/src/main/java/com/example/helloworld/activities/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/activities/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.helloworld.R
+import com.example.helloworld.databinding.ActivityMainBinding
 import com.example.helloworld.ui.HelloViewModel
 
 //Note that we removed the AppView interface and the Presenter class
@@ -17,22 +18,24 @@ class MainActivity : AppCompatActivity() {
     //View [MainActivity containing TextView & Button] - UI
     //ViewModel [HelloViewModel] - Logic
 
-    var button: Button? = null
-    var textView : TextView? = null
-    var viewModel: HelloViewModel? = null
+    //Removed view variables
+    private var viewModel: HelloViewModel? = null
+
+    //View binding variable - https://developer.android.com/topic/libraries/view-binding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        button = findViewById(R.id.button3)
-        textView = findViewById(R.id.textView)
+        //Bind view
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //Set the visible view to the bound view
+        setContentView(binding.root)
 
         //Initialize the ViewModel
         viewModel = ViewModelProvider(this)[HelloViewModel::class.java]
 
         //Add a click listener to the button
-        button?.setOnClickListener(View.OnClickListener {
+        binding.button3.setOnClickListener(View.OnClickListener {
             //Call to update the data in the view model
             //The Activity no longer contains the logic binding the view to the model
             viewModel!!.onGetAppName()
@@ -41,7 +44,8 @@ class MainActivity : AppCompatActivity() {
         //Observing the data for any changes
         //Once a change is detected, the UI is updated
         viewModel!!.mutableLiveData.observe(this) {
-            textView?.text = it
+            //binding provides nullsafety for the view
+            binding.textView.text = it
         }
     }
 }
